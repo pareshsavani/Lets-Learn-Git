9f0400677f29c150a4d850473fbc28c546dac167
Paresh savani <pareshsavani@gmail.com>
2022-04-10 19:14:26 -0400

    added message in sum.java

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
 public class sum{
-    publi static void main(String[] args){
+    public static void main(String[] args){
         int a=5;
         int b=6;
 
-        System.out.println(a+b);
+        System.out.println("This is sun"+(a+b));
     }
 
 }
